7725533ceeeb668c45799b0a1e826e9947504e3a
CésarM <61838637+0Xero0@users.noreply.github.com>
2024-05-06 17:11:25 -0500

    Corrección validar tamaño archivos
    Corrige la validación del tamaño de archivos, comparando el tamaño en binario y no en base10

--- src/app/administrar-polizas/componentes/polizas/validadores/tamanio-archivo-validar.ts
+++ src/app/administrar-polizas/componentes/polizas/validadores/tamanio-archivo-validar.ts
@@ -1,9 +1,11 @@
 
 export function tamanioValido(archivo: File, tamanioMaximoMb: number){
-  const tm  = tamanioMaximoMb * 1000000
+  const tm  = tamanioMaximoMb * 1048576
   if(archivo.size > tm){
+    console.log(archivo.size, " > " ,tm);
     return true
   }else{
+    console.log(archivo.size, " < " ,tm);
     return false
   }
 }
