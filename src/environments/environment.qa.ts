3b7f6e5eb0100e2d9ff8d08381baca6f7321a7af
CésarM <61838637+0Xero0@users.noreply.github.com>
2024-06-17 16:31:05 -0500

    Cambio enviroment

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://172.16.3.104:3001',
+  urlBackend: 'http://172.16.2.104:3001',
   urlBackendVigia: 'http://172.16.3.104:3334',
   urlBackendArchivos: 'http://172.16.3.104:3001',
   tokenBackendArchivos: 'd4a32a3b-def6-4cc2-8f77-904a67360b53',
