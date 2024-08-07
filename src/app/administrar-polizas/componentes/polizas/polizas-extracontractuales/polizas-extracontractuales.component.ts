95363cb745e1310d380755daa4d4f93666729d76
kevinsoto772 <kosotosimanca@gmail.com>
2024-08-06 20:24:33 -0500

    visualizar formularios polizas

--- src/app/administrar-polizas/componentes/polizas/polizas-extracontractuales/polizas-extracontractuales.component.ts
+++ src/app/administrar-polizas/componentes/polizas/polizas-extracontractuales/polizas-extracontractuales.component.ts
@@ -1,21 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { maxLengthNumberValidator } from '../validadores/maximo-validador';
 import { valorCeroValidar } from '../validadores/cero-validacion';
 import { negativoValidar } from '../validadores/negativo-verificar';
 import { capasValidator } from '../validadores/capas-validacion';
-import { HttpErrorResponse } from '@angular/common/http';
-import Swal from 'sweetalert2';
-import { tamanioValido } from '../validadores/tamanio-archivo-validar';
-import { marcarFormularioComoSucio } from 'src/app/administrador/utilidades/Utilidades';
-import { CaratulaModel, PolizaContractualModel, PolizaExtracontractualModel, ResponsabilidadModel } from 'src/app/administrar-polizas/modelos/guardarPoliza';
-import { fechaValida } from '../validadores/fecha-validador';
 import { ServicioAdministrarPolizas } from 'src/app/administrar-polizas/servicios/administrar-polizas.service';
 import { amparos } from 'src/app/administrar-polizas/modelos/amparos';
 import { Aseguradoras } from 'src/app/administrar-polizas/modelos/aseguradoras';
-import { ArchivoGuardado } from 'src/app/archivos/modelos/ArchivoGuardado';
-import { PopupComponent } from 'src/app/alertas/componentes/popup/popup.component';
-import { ModalCapacidadComponent } from '../modal/modal-capacidad/modal-capacidad.component';
+
 
 @Component({
   selector: 'app-polizas-extracontractuales',
@@ -25,20 +17,15 @@ import { ModalCapacidadComponent } from '../modal/modal-capacidad/modal-capacida
 export class PolizasExtracontractualesComponent {
 
   base64String!: string
-  archivoCargado: string = ""
-  archivoPDF?: ArchivoGuardado
   obligatorio: boolean = false
-  deshabilitado: boolean = false
 
-  desplegarRCC: boolean = true
   desplegarRCE: boolean = true
   desplegarAmparosB1: boolean = true
   desplegarAmparosA1: boolean = true
   desplegarAmparosB2: boolean = true
   desplegarAmparosA2: boolean = true
-  fondoResponsabilidadC: boolean = false
   fondoResponsabilidadE: boolean = false
-
+  polizaLocalStorage = localStorage.getItem('poliza');
   aseguradoras: Aseguradoras[] = []
 
   amparosBasicosC: amparos[] = [
@@ -110,69 +97,16 @@ export class PolizasExtracontractualesComponent {
       capa2: new FormControl(undefined),
     })
     this.formExtracontractual.get('checkResponsabilidadE')?.disable()
+    this.formExtracontractual.disable()
   }
 
   ngOnInit(): void {
-    this.deshabilitarFormularios()
-    this.obtenerAseguradora()
-  }
-  fechasVerificar(tipoPoliza: number) {
-
-    const controlE = this.formExtracontractual.controls
-      if (tipoPoliza == 2) {
-      if (controlE['vigenciaPolizaInicioE'].value && controlE['vigenciaPolizaFinalE'].value) {
-        if (controlE['vigenciaPolizaInicioE'].value >= controlE['vigenciaPolizaFinalE'].value) {
-          controlE['vigenciaPolizaInicioE'].setValue('')
-          controlE['vigenciaPolizaFinalE'].setValue('')
-          Swal.fire({
-            titleText: "El inicio de la fecha de vigencia no puede ser posterior o igual a la fecha final de vigencia.",
-            icon: "error"
-          })
-        }
-      }
-    }
+    this.obtenerAseguradora();
+    this.llenarFormulario();
   }
 
-  fechaValida(event: any, tipoPoliza: number) {
-    if (fechaValida(event)) {
-      Swal.fire({
-        titleText: "La fecha ingresada no puede ser superior o igual a la fecha actual",
-        icon: "error"
-      })
-      console.log(event.target.name);
-      if (tipoPoliza == 2) {
-        this.formExtracontractual.controls[event.target.name].setValue('')
-      }
-    }
-  }
-
-  respuestaNoResponsabilidad(tipoPoliza: number) {
-    const controlE = this.formExtracontractual.controls
-    if (tipoPoliza == 2) {
-      //console.log(controlC['checkNoResponsabilidadC'].value);
-      if (controlE['checkNoResponsabilidadE'].value) {
-        controlE['checkResponsabilidadE'].disable()
-        controlE['checkResponsabilidadE'].setValue(false)
-      } else {
-        controlE['checkResponsabilidadE'].enable()
-        controlE['checkResponsabilidadE'].setValue(false)
-      }
-    }
-  }
-
-  deshabilitarFormularios() {
-    this.servicioAdministrarPoliza.obtenerEstadoVigilado().subscribe({
-      next: (deshabilitado: any) => {
-        if (deshabilitado.enviado) {
-          this.deshabilitado = deshabilitado.enviado
-          this.formExtracontractual.disable()
-          Swal.fire({
-            titleText: "¡Usted ya envió a la Superintendencia de Transporte",
-            icon: "info"
-          })
-        }
-      }
-    })
+  formatearFecha(fechaString: string): string {
+    return new Date(fechaString).toISOString().split('T')[0];
   }
 
   obtenerAseguradora() {
@@ -183,27 +117,68 @@ export class PolizasExtracontractualesComponent {
     })
   }
 
-  cambiarNumeroPoliza(tipoPoliza: number) {
-    if (tipoPoliza == 2) { this.formExtracontractual.controls['cargarExcel'].setValue('') }
+  llenarFormulario() {
+    if (this.polizaLocalStorage) {
+      const poliza = JSON.parse(this.polizaLocalStorage);
+      const controlC = this.formExtracontractual.controls;
+    //-- Responsabilidad contractual
+        controlC['numeroPolizaE'].setValue(poliza.numero);
+        controlC['aseguradorasE'].setValue(poliza.aseguradoraId);
+        controlC['vigenciaPolizaInicioE'].setValue(this.formatearFecha(poliza.inicioVigencia));
+        controlC['vigenciaPolizaFinalE'].setValue(this.formatearFecha(poliza.finVigencia));
+      //----- Amparos basicos -----//
+        controlC['valorAseguradoAB9'].setValue(poliza.amparos[0].valorAsegurado);
+        controlC['limitesAB9'].setValue(poliza.amparos[0].limite);
+        controlC['deducibleAB9'].setValue(poliza.amparos[0].deducible);
+        controlC['valorAseguradoAB10'].setValue(poliza.amparos[1].valorAsegurado);
+        controlC['limitesAB10'].setValue(poliza.amparos[1].limite);
+        controlC['deducibleAB10'].setValue(poliza.amparos[1].deducible);
+        controlC['valorAseguradoAB11'].setValue(poliza.amparos[2].valorAsegurado);
+        controlC['limitesAB11'].setValue(poliza.amparos[2].limite);
+        controlC['deducibleAB11'].setValue(poliza.amparos[2].deducible);
+      //----- Amparos adicionales -----//
+        controlC['valorAseguradoAA12'].setValue(poliza.amparos[3].valorAsegurado);
+        controlC['limitesAA12'].setValue(poliza.amparos[3].limite);
+        controlC['deducibleAA12'].setValue(poliza.amparos[3].deducible);
+        controlC['valorAseguradoAA13'].setValue(poliza.amparos[4].valorAsegurado);
+        controlC['limitesAA13'].setValue(poliza.amparos[4].limite);
+        controlC['deducibleAA13'].setValue(poliza.amparos[4].deducible);
+        controlC['valorAseguradoAA14'].setValue(poliza.amparos[5].valorAsegurado);
+        controlC['limitesAA14'].setValue(poliza.amparos[5].limite);
+        controlC['deducibleAA14'].setValue(poliza.amparos[5].deducible);
+        controlC['valorAseguradoAA15'].setValue(poliza.amparos[6].valorAsegurado);
+        controlC['limitesAA15'].setValue(poliza.amparos[6].limite);
+        controlC['deducibleAA15'].setValue(poliza.amparos[6].deducible);
+
+      //----- Responsabilidad -----//
+      if (poliza.responsabilidad == true) {
+        // controlC['checkResponsabilidadE'].enable();
+        controlC['checkResponsabilidadE'].setValue(poliza.responsabilidad);
+        controlC['checkNoResponsabilidadE'].setValue(false);
+        this.DesplegarFondoResponsabilidad();
+
+        controlC['fechaConstitucion'].setValue(this.formatearFecha(poliza.responsabilidades.fechaConstitucion));
+        controlC['numeroResolucion'].setValue(poliza.responsabilidades.resolucion);
+        controlC['fechaResolucion'].setValue(this.formatearFecha(poliza.responsabilidades.fechaResolucion));
+        controlC['valorReserva'].setValue(poliza.responsabilidades.valorReserva);
+        controlC['fechaCorteReserva'].setValue(this.formatearFecha(poliza.responsabilidades.fechaReserva));
+        controlC['infoComplementaria'].setValue(poliza.responsabilidades.informacion);
+        controlC['capas'].setValue(poliza.responsabilidades.operacion);
+        controlC['capa1'].setValue(poliza.responsabilidades.valorCumplimiento_uno);
+        controlC['capa2'].setValue(poliza.responsabilidades.valorCumplimiento_dos);
+      } else {
+        controlC['checkResponsabilidadE'].setValue(false);
+        // controlC['checkNoResponsabilidadE'].enable();
+        controlC['checkNoResponsabilidadE'].setValue(true);
+      }
+
+    } else {
+      console.error('polizaLocalStorage es null o undefined.');
+    }
   }
 
-  alternarDesplegarRCC() {
-    this.desplegarRCC = !this.desplegarRCC
-  }
-  alternarDesplegarRCE() {
-    this.desplegarRCE = !this.desplegarRCE
-  }
-  alternarDesplegarAB(tipoPoliza: number) {
-    if (tipoPoliza == 1) { this.desplegarAmparosB1 = !this.desplegarAmparosB1 }
-    if (tipoPoliza == 2) { this.desplegarAmparosB2 = !this.desplegarAmparosB2 }
-  }
-  alternarDesplegarAA(tipoPoliza: number) {
-    if (tipoPoliza == 1) { this.desplegarAmparosA1 = !this.desplegarAmparosA1 }
-    if (tipoPoliza == 2) { this.desplegarAmparosA2 = !this.desplegarAmparosA2 }
-  }
 
-  DesplegarFondoResponsabilidad(tipoPoliza: number) {
-    if (tipoPoliza == 2) {
+  DesplegarFondoResponsabilidad() {
       if (this.formExtracontractual.controls['checkResponsabilidadE'].value) {
         this.fondoResponsabilidadE = this.formExtracontractual.controls['checkResponsabilidadE'].value
         this.formExtracontractual.get('checkNoResponsabilidadE')?.disable()
@@ -241,80 +216,6 @@ export class PolizasExtracontractualesComponent {
         this.formExtracontractual.get('capa1')?.reset()
         this.formExtracontractual.get('capa2')?.reset()
       }
-    }
   }
 
-  numeroPolizaELleno() {
-    if (this.formExtracontractual.controls['numeroPolizaE'].value) {
-      this.obligatorio = true
-      this.formExtracontractual.get('checkResponsabilidadE')?.enable(); this.formExtracontractual.get('checkNoResponsabilidadE')?.enable()
-      this.formExtracontractual.get('numeroPolizaE')?.setValidators([Validators.required, maxLengthNumberValidator(18), valorCeroValidar(), negativoValidar()]); this.formExtracontractual.get('numeroPolizaE')?.updateValueAndValidity()
-      this.formExtracontractual.get('aseguradorasE')?.setValidators([Validators.required]); this.formExtracontractual.get('aseguradorasE')?.updateValueAndValidity()
-      this.formExtracontractual.get('vigenciaPolizaInicioE')?.setValidators([Validators.required]); this.formExtracontractual.get('vigenciaPolizaInicioE')?.updateValueAndValidity()
-      this.formExtracontractual.get('vigenciaPolizaFinalE')?.setValidators([Validators.required]); this.formExtracontractual.get('vigenciaPolizaFinalE')?.updateValueAndValidity()
-      //------ Amparos basicos
-      /* this.formExtracontractual.get('valorAseguradoAB9')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB9')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB9')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAB10')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB10')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB10')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAB11')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAB11')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB11')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAB11')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB11')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAB11')?.updateValueAndValidity()
-       *///------- Amparos Adicionales
-      /* this.formExtracontractual.get('valorAseguradoAA12')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA12')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA12')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA13')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA13')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA13')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA14')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA14')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA14')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA15')?.setValidators([ Validators.required ]); this.formExtracontractual.get('valorAseguradoAA15')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA15')?.setValidators([ Validators.required ]); this.formExtracontractual.get('limitesAA15')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA15')?.setValidators([ Validators.required ]); this.formExtracontractual.get('deducibleAA15')?.updateValueAndValidity()
-       *///------ Cargar Archivos
-      this.formExtracontractual.get('cargarExcel')?.setValidators([Validators.required]); this.formExtracontractual.get('cargarExcel')?.updateValueAndValidity()
-      this.formExtracontractual.get('cargarPDF')?.setValidators([Validators.required]); this.formExtracontractual.get('cargarPDF')?.updateValueAndValidity()
-    } else {
-      this.obligatorio = false
-      this.formExtracontractual.get('checkResponsabilidadE')?.disable(); this.formExtracontractual.get('checkNoResponsabilidadE')?.disable()
-      this.formExtracontractual.get('numeroPolizaE')?.clearValidators(); this.formExtracontractual.get('numeroPolizaE')?.updateValueAndValidity()
-      this.formExtracontractual.get('aseguradorasE')?.clearValidators(); this.formExtracontractual.get('aseguradorasE')?.updateValueAndValidity()
-      this.formExtracontractual.get('vigenciaPolizaInicioE')?.clearValidators(); this.formExtracontractual.get('vigenciaPolizaInicioE')?.updateValueAndValidity()
-      this.formExtracontractual.get('vigenciaPolizaFinalE')?.clearValidators(); this.formExtracontractual.get('vigenciaPolizaFinalE')?.updateValueAndValidity()
-      //------ Amparos basicos
-      /* this.formExtracontractual.get('valorAseguradoAB9')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB9')?.clearValidators(); this.formExtracontractual.get('limitesAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB9')?.clearValidators(); this.formExtracontractual.get('deducibleAB9')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAB10')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB10')?.clearValidators(); this.formExtracontractual.get('limitesAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB10')?.clearValidators(); this.formExtracontractual.get('deducibleAB10')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAB11')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAB11')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAB11')?.clearValidators(); this.formExtracontractual.get('limitesAB11')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAB11')?.clearValidators(); this.formExtracontractual.get('deducibleAB11')?.updateValueAndValidity()
-       *///------- Amparos Adicionales
-      /* this.formExtracontractual.get('valorAseguradoAA12')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA12')?.clearValidators(); this.formExtracontractual.get('limitesAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA12')?.clearValidators(); this.formExtracontractual.get('deducibleAA12')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA13')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA13')?.clearValidators(); this.formExtracontractual.get('limitesAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA13')?.clearValidators(); this.formExtracontractual.get('deducibleAA13')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA14')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA14')?.clearValidators(); this.formExtracontractual.get('limitesAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA14')?.clearValidators(); this.formExtracontractual.get('deducibleAA14')?.updateValueAndValidity()
-      this.formExtracontractual.get('valorAseguradoAA15')?.clearValidators(); this.formExtracontractual.get('valorAseguradoAA15')?.updateValueAndValidity()
-      this.formExtracontractual.get('limitesAA15')?.clearValidators(); this.formExtracontractual.get('limitesAA15')?.updateValueAndValidity()
-      this.formExtracontractual.get('deducibleAA15')?.clearValidators(); this.formExtracontractual.get('deducibleAA15')?.updateValueAndValidity()
-       *///------ Cargar Archivos
-      this.formExtracontractual.get('cargarExcel')?.clearValidators(); this.formExtracontractual.get('cargarExcel')?.updateValueAndValidity()
-      this.formExtracontractual.get('cargarPDF')?.clearValidators(); this.formExtracontractual.get('cargarPDF')?.updateValueAndValidity()
-
-      this.formExtracontractual.controls['checkResponsabilidadE'].setValue(false); this.formExtracontractual.get('checkResponsabilidadE')?.updateValueAndValidity()
-      this.formExtracontractual.controls['checkNoResponsabilidadE'].setValue(false); this.formExtracontractual.get('checkNoResponsabilidadE')?.updateValueAndValidity()
-      this.DesplegarFondoResponsabilidad(2)
-    }
-  }
 }
