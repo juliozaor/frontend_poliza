4f6a260004f4779fa77cb54f1d5a9b7fabb4531a
kevinsoto772 <kosotosimanca@gmail.com>
2024-08-15 11:45:28 -0500

    Actualizacion tabla vincular vehiculo a poliza

--- src/app/administrar-polizas/componentes/gestionar-polizas/gestionar-polizas.component.ts
+++ src/app/administrar-polizas/componentes/gestionar-polizas/gestionar-polizas.component.ts
@@ -6,7 +6,10 @@ import { Paginacion } from 'src/app/compartido/modelos/Paginacion';
 import { Observable } from 'rxjs';
 import { ServicioAdministrarPolizas } from '../../servicios/administrar-polizas.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FiltrosNovedadesPoliza } from '../../modelos/FiltrosNovedadesPoliza';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { valorCeroValidar } from '../polizas/validadores/cero-validacion';
+import { negativoValidar } from '../polizas/validadores/negativo-verificar';
+import { marcarFormularioComoSucio } from 'src/app/administrador/utilidades/Utilidades';
 
 @Component({
   selector: 'app-gestionar-polizas',
@@ -16,24 +19,46 @@ import { FiltrosNovedadesPoliza } from '../../modelos/FiltrosNovedadesPoliza';
 export class GestionarPolizasComponent {
   polizas: Array<{ poliza: string, tipoPoliza: string | number, estadoPoliza: string, fechaInicio: string, fechaFin: string, fechaCargue: string, aseguradora: string, cantidadVehiculos: string }> = []
   novedadesPoliza: Array<{ poliza: string, tipoPoliza: string | number, placa: string, fechaActualizacion: string, estado: string, observacion: string }> = []
+  novedadesPolizaPaginacion: Array<{ poliza: string, tipoPoliza: string | number, placa: string, fechaActualizacion: string, estado: string, observacion: string }> = []
+  placasInteroperabilidad: Array<{ placas: string}> = []
+  placasInteroperabilidadPaginacion: Array<{ placas: string}> = []
+
   tipoPoliza: number = 0;
   numeroPoliza: any;
   verPoliza: boolean = false;
   poliza:any;
+  placas: [] = [];
+  habilitarVinculacion: Boolean = false;
+  
+  formPasajeros: FormGroup;
 
   paginador: Paginador<FiltrarPolizas>
-  // paginador2: Paginador<{ placas : string }>
-  // paginador3: Paginador<FiltrosNovedadesPoliza>
+
+  paginadorNovedades = {
+    totalRegistros: 0,
+    pagina: 1,
+    limite: 5,
+  };
+
+  paginadorInteroperabilidad = {
+    totalRegistros: 0,
+    pagina: 1,
+    limite: 5,
+  };
+
   constructor(private servicioAut: AutenticacionService,
     private servicioAdministrarPoliza: ServicioAdministrarPolizas,
     private activatedRoute: ActivatedRoute,
     private router: Router,) {
     this.paginador = new Paginador<FiltrarPolizas>(this.obtenerPolizas)
-    // this.paginador2 = new Paginador<{ placas : string }>()
+    this.formPasajeros = new FormGroup({
+      pasajeros: new FormControl(undefined, [Validators.required, valorCeroValidar(), negativoValidar()]),
+    })
   }
   
   ngOnInit(): void {
     this.inicializarPaginador();
+    this.formPasajeros.controls['pasajeros'].disable();
   }
 
   obtenerPolizas = (pagina: number, limite: number, filtros?: FiltrarPolizas) => {
@@ -42,7 +67,6 @@ export class GestionarPolizasComponent {
         next: (polizas: any) => {
           this.polizas = polizas.polizas
           suscriptor.next(polizas.paginacion)
-          console.log(this.polizas);
         }
       })
     })
@@ -71,7 +95,8 @@ export class GestionarPolizasComponent {
       next: (poliza: any) => {
         this.poliza = poliza
         this.verPoliza = true;
-        this.visualizarNovedadesPolizas(numero, tipoPoliza)
+        this.visualizarNovedadesPolizas(numero, tipoPoliza);
+        this.obtenerInteroperabilidad(numero, tipoPoliza);
       }
     })
   }
@@ -83,10 +108,65 @@ export class GestionarPolizasComponent {
     this.servicioAdministrarPoliza.consultarNovedadesPoliza(idTipo, numero).subscribe({
       next: (novedades: any) => {
         this.novedadesPoliza = novedades;
+        this.paginadorNovedades.totalRegistros = this.novedadesPoliza.length;
+        this.actualizarNovedadesPaginadas()
       }
     })
   }
 
+  obtenerInteroperabilidad(numero: any, tipoPoliza: any) {
+    let idTipo; 
+    if(tipoPoliza === 'RESPONSABILIDAD CIVIL CONTRACTUAL')idTipo = 1, this.tipoPoliza = 1
+    if (tipoPoliza === 'RESPONSABILIDAD CIVIL EXTRACONTRACTUAL') idTipo = 2, this.tipoPoliza = 2 
+    this.servicioAdministrarPoliza.consultarInteroperabilidad(idTipo, numero).subscribe({
+      next: (placas: any) => {
+        this.placasInteroperabilidad = placas.placasDisponibles;
+        this.paginadorInteroperabilidad.totalRegistros = this.placasInteroperabilidad.length;
+        console.log(this.paginadorInteroperabilidad);
+        this.actualizarInteroperabilidadPaginadas()
+      }
+    })
+  }
 
+  habilitarVinculacionVehiculo(event: Event, index: number) {
+    const checkbox = event.currentTarget as HTMLInputElement
+    if (checkbox.checked == true) {
+      this.formPasajeros.controls['pasajeros'].enable();
+    } else {
+      this.formPasajeros.controls['pasajeros'].disable();
+    }
+  }
+
+  agregarVehiculosPoliza() {
+    if (this.formPasajeros.invalid) {
+      marcarFormularioComoSucio(this.formPasajeros);
+      return;
+    }
+    const vincularVehiculoPoliza = {
+
+    }
+  }
+
+  actualizarInteroperabilidadPaginadas() {
+    const startIndex = (this.paginadorInteroperabilidad.pagina - 1) * this.paginadorInteroperabilidad.limite;
+    const endIndex = startIndex + this.paginadorInteroperabilidad.limite;
+    this.placasInteroperabilidadPaginacion = this.placasInteroperabilidad.slice(startIndex, endIndex);     
+  }
+  
+  cambiarPaginaInteroperabilidad(pagina: number) {
+    this.paginadorInteroperabilidad.pagina = pagina;
+    this.actualizarInteroperabilidadPaginadas();
+  }
+
+  actualizarNovedadesPaginadas() {
+    const startIndex = (this.paginadorNovedades.pagina - 1) * this.paginadorNovedades.limite;
+    const endIndex = startIndex + this.paginadorNovedades.limite;
+    this.novedadesPolizaPaginacion = this.novedadesPoliza.slice(startIndex, endIndex); // Obtener solo las novedades para la página actual
+  }
+  // Cambia de página cuando se detecta un cambio
+  cambiarPaginaNovedades(pagina: number) {
+    this.paginadorNovedades.pagina = pagina;
+    this.actualizarNovedadesPaginadas()  // Carga los datos para la nueva página
+  }
 
 }
